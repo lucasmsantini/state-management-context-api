44b3c5872c7b3c41fd5f0b8e9a1fd2efc5dc941b
Lucas <lucas.santini@nddigital.com.br>
2020-10-27 13:57:37 -0300

    AddMovie with Prices are work correctly

--- src/AddMovie.js
+++ src/AddMovie.js
@@ -1,19 +1,31 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
+import {MovieContext} from './MovieContext.js';
 
 const AddMovie = () => {
     const [name,setName] = useState('');
     const [price,setPrice] = useState('');
+    const [movies,setMovies] = useContext(MovieContext);
     
-const updateName = (e) => {
-    setName(e.target.value);
-}
+    const updateName = (e) => {
+        setName(e.target.value);
+    }
+
+    const updatePrice = (e) => {
+        setPrice(e.target.value);
+    }
+
+    const addMovie = e => {
+        e.preventDefault();
+        setMovies(prevMovies => [...prevMovies, {name: name, price: price}])
+    };
 
     return(
-        <form>
-            <input type="text" name="name"value={name} onChange={updateName}/>
-            <input type="text" name="price" />
+        <form onSubmit={addMovie}>
+            <input type="text" name="name" value={name} onChange={updateName}/>
+            <input type="text" name="price" value={price} onChange={updatePrice} />
             <button>Submit</button>
         </form>
-    )
+    );
 };
+
 export default AddMovie;
--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import MovieList from './MovieList.js';
 import Nav from './Nav.js'
 import {MovieProvider} from './MovieContext';
+import AddMovie from './AddMovie.js'
 
 function App() {
   return (
     <MovieProvider>
       <div className="App">
         <Nav />
+        <AddMovie />
         <MovieList />
       </div>
     </MovieProvider>
